ab219593d135ff9b1e4c75ac0a57d185fd04cc2b
Yuankai619 <Yuankai_@outlook.com>
2023-12-09 04:11:24 +0800

    update Signup page
    完成App.jsx 並且開始一點點分開component的動作(TextField)

--- signup-page/src/components/App.jsx
+++ signup-page/src/components/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-// import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Box from '@mui/material/Box';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider ,useTheme } from '@mui/material/styles';
 const TextFieldtheme = createTheme({
+    typography: {
+      fontFamily: [
+        'Poppins', // 您選擇的 Google 字體
+        'sans-serif', // 作為後備的系統字體
+      ].join(','),
+    },
     components: {
       MuiTextField: {
         styleOverrides: {
           root: {
             input: {
+              // borderRadius: 8,
               color: '#EEEEEE', // 文字顏色
             },
+            
             '& label.Mui-focused': {
               color: '#EEEEEE', // 聚焦時的 label 顏色
             },
@@ -34,106 +44,156 @@ const TextFieldtheme = createTheme({
           },
         },
       },
+      MuiCheckbox:{
+        styleOverrides:{
+          root:{
+            color: '#999999',
+            '&$checked': {
+              color: '#EEEEEE',  // 選中時的框線顏色
+            },
+          }
+        }
+      },
+      MuiFormGroup:{
+        styleOverrides:{
+          root: {
+            '& .MuiFormControlLabel-label': {
+              color: '#EEEEEE', // 文字顏色
+            }
+          },
+        }
+      },
+
+      MuiButton:{
+        styleOverrides:{
+          root: {
+            borderRadius: 6,
+            backgroundColor: '#EEEEEE', // 背景顏色
+            color: '#2D6CB6', // 文字顏色
+            '&:hover': {
+              backgroundColor: '#EEEEEE',
+              color: '#2D6CB6',
+            },
+          },
+        }
+      },
     },
   });
   
 function App() {
-
-    return (
-        <div>
-        <Container maxWidth="sm">
-            <Box my={2}>
-            <h1 className="title">Create account</h1>
-            </Box>
-            <Box my={2}>
+  const curtheme = useTheme();
+  const isMobile = useMediaQuery(curtheme.breakpoints.down('sm'));
+  const isPad = useMediaQuery(curtheme.breakpoints.down('md'));
+  const boxGap = "45px";
+  return (
+      <div>
+      <ThemeProvider theme={TextFieldtheme}>
+      <Container maxWidth="sm" sx={{py:"75px", px: isMobile ? "45px":(isPad ? "144px":"360px") }}>
+          <Box my={boxGap}>
+          <h1 className="title">Create account</h1>
+          </Box>
+          <Box my={boxGap}>
+          <ThemeProvider theme={TextFieldtheme}>
+              <TextField
+                  // focused 
+                  fullWidth 
+                  required
+                  id="outlined-required"
+                  label="username"
+                  defaultValue=""
+                  // className="input-field"
+                  // color="#warning"
+                  />
+          </ThemeProvider>
+          </Box>
+          <Box my={boxGap}>
+          <ThemeProvider theme={TextFieldtheme}>
+              <TextField
+                  // focused 
+                  fullWidth 
+                  required
+                  id="outlined-required"
+                  label="password"
+                  type="password"
+                  defaultValue=""
+                  // className="input-field"
+                  // color="#warning"
+                  />
+          </ThemeProvider>
+          </Box>
+          <Box my={boxGap}>
+          <ThemeProvider theme={TextFieldtheme}>
+              <TextField
+                  // focused 
+                  required
+                  fullWidth 
+                  id="outlined-required"
+                  label="confirm password"
+                  type="password"
+                  defaultValue=""
+                  // className="input-field"
+                  // color="#warning"
+                  />
+          </ThemeProvider>
+          </Box>
+          <Box my={boxGap}>
+          <ThemeProvider theme={TextFieldtheme}>
+              <TextField
+                  // focused 
+                  required
+                  fullWidth 
+                  id="outlined-required"
+                  label="real name"
+                  defaultValue=""
+                  // className="input-field"
+                  // color="#warning"
+                  />
+          </ThemeProvider>
+          </Box>
+          <Box my={boxGap}>
+          <ThemeProvider theme={TextFieldtheme}>
+              <TextField
+                  // focused 
+                  required
+                  fullWidth 
+                  id="outlined-required"
+                  label="student ID"
+                  defaultValue=""
+                  // className="input-field"
+                  // color="#warning"
+                  />
+          </ThemeProvider>
+          </Box>
+          <Box my={boxGap}>
+          <ThemeProvider theme={TextFieldtheme}>
+              <TextField
+                  // focused 
+                  required
+                  fullWidth 
+                  id="outlined-required"
+                  label="email"
+                  defaultValue=""
+                  // className="input-field"
+                  // color="#warning"
+                  />
+          </ThemeProvider>
+          </Box >
+          <Box my={boxGap}>
             <ThemeProvider theme={TextFieldtheme}>
-                <TextField
-                    // focused 
-                    fullWidth 
-                    required
-                    id="outlined-required"
-                    label="username"
-                    defaultValue=""
-                    // className="input-field"
-                    // color="#warning"
-                    />
+            <FormGroup>
+              <FormControlLabel control={<Checkbox />}   label="I accept the terms and privacy policy" />
+            </FormGroup>
             </ThemeProvider>
-            </Box>
-            <Box my={2}>
+          </Box>
+          <Box sx={{my:boxGap,px:"40px"}}>
             <ThemeProvider theme={TextFieldtheme}>
-                <TextField
-                    // focused 
-                    fullWidth 
-                    required
-                    id="outlined-required"
-                    label="password"
-                    type="password"
-                    defaultValue=""
-                    // className="input-field"
-                    // color="#warning"
-                    />
+            <Button variant="text" fullWidth  >SignUp</Button>
             </ThemeProvider>
-            </Box>
-            <Box my={2}>
-            <ThemeProvider theme={TextFieldtheme}>
-                <TextField
-                    // focused 
-                    required
-                    fullWidth 
-                    id="outlined-required"
-                    label="confirm password"
-                    type="password"
-                    defaultValue=""
-                    // className="input-field"
-                    // color="#warning"
-                    />
-            </ThemeProvider>
-            </Box>
-            <Box my={2}>
-            <ThemeProvider theme={TextFieldtheme}>
-                <TextField
-                    // focused 
-                    required
-                    fullWidth 
-                    id="outlined-required"
-                    label="real name"
-                    defaultValue=""
-                    // className="input-field"
-                    // color="#warning"
-                    />
-            </ThemeProvider>
-            </Box>
-            <Box my={2}>
-            <ThemeProvider theme={TextFieldtheme}>
-                <TextField
-                    // focused 
-                    required
-                    fullWidth 
-                    id="outlined-required"
-                    label="student ID"
-                    defaultValue=""
-                    // className="input-field"
-                    // color="#warning"
-                    />
-            </ThemeProvider>
-            </Box>
-            <Box my={2}>
-            <ThemeProvider theme={TextFieldtheme}>
-                <TextField
-                    // focused 
-                    required
-                    fullWidth 
-                    id="outlined-required"
-                    label="email"
-                    defaultValue=""
-                    // className="input-field"
-                    // color="#warning"
-                    />
-            </ThemeProvider>
-            </Box >
-        </Container>
-        </div>
-    );
+          </Box>
+      </Container>
+      </ThemeProvider>
+      </div>
+  );
 }
 
 export default App;
