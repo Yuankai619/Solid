316d13d35b1c683c66069e7ec39f8d942b61775c
Yuankai619 <Yuankai_@outlook.com>
2023-12-15 04:11:10 +0800

    add rouer

--- solid-main/src/components/App.jsx
+++ solid-main/src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import SignupPage from "../pages/Signup";
 import LoginPage from "../pages/Login";
-//import Home from "../pages/Home";
+import HomePage from "../pages/Home"; 
+// import GetUserState from "../../backend/passportConfig";
+import {
+  BrowserRouter as Router, Route, Routes
+} from "react-router-dom";
+
 function App() {
   return (
-      // <Home></Home>
-      // <SignupPage></SignupPage>
-      <LoginPage></LoginPage>
+      <Routes>
+        <Route path="/" Component={LoginPage}/>
+        <Route path="/login" Component={LoginPage}/>
+        <Route path="/signup" Component={SignupPage}/>
+        <Route path="/home" Component={HomePage}/> 
+      </Routes>
   );
 }
 
--- solid-main/src/index.js
+++ solid-main/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-
+import { BrowserRouter } from 'react-router-dom';
 // import TextField from '@mui/material/TextField';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
