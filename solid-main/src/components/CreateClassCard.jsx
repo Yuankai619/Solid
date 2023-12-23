8ec2a078baca8b4c15c63215a10bf87b026f1356
Yuankai619 <Yuankai_@outlook.com>
2023-12-24 04:54:51 +0800

    update CreatedClassCard style

--- solid-main/src/components/CreateClassCard.jsx
+++ solid-main/src/components/CreateClassCard.jsx
@@ -14,14 +14,17 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CardHeader } from "@mui/material";
 import CreateClassCardTheme from '../themes/CreateClassCardTheme';
 import { Link } from 'react-router-dom';
-import { useClassDataContext } from '../context/ClassDataContext'; 
+import { useClassDataContext } from '../context/ClassDataContext';
 
-function CreateClassCard({data}) {
+function CreateClassCard({ data }) {
   const { handleChangeCreatedClassState, handleDeleteCreatedClass } = useClassDataContext();
   const navigate = useNavigate();
 
   const [stateColor, setStatecolor] = useState('#000');
   const [menuState, setMenuState] = useState(data.state);
+  const [allowNavigate, setAllowNavigate] = useState(true); // 新增状态来控制是否允许跳转
+
+
   useEffect(() => {
     data.state == 'true' ? setStatecolor('#2D6CB6') : setStatecolor('#999999');
   }, [data.state]);
@@ -30,72 +33,81 @@ function CreateClassCard({data}) {
   const open = Boolean(anchorEl);
 
   const handleClickMenu = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
     setAnchorEl(event.currentTarget);
+    setAllowNavigate(false); // 打开 Menu 时禁止跳转F
   };
 
   const handleClose = () => {
     setAnchorEl(null);
+    setAllowNavigate(true);
   };
 
   const handleChangeState = (event) => {
     event.stopPropagation();
     setMenuState(menuState == 'true' ? 'false' : 'true');
-    handleChangeCreatedClassState(data.id, menuState == 'true'? 'false' : 'true');
+    handleChangeCreatedClassState(data.id, menuState == 'true' ? 'false' : 'true');
   };
   const handleDelete = (event) => {
     event.stopPropagation();
     console.log(data.id)
-    handleDeleteCreatedClass(data.id);    
+    handleDeleteCreatedClass(data.id);
   };
   return (
     <ThemeProvider theme={CreateClassCardTheme}>
       <Box sx={{ minWidth: 275 }} >
-        <Card variant="outlined" sx={{ cursor: 'pointer' }} > 
-          <CardHeader
-            action={
-              <IconButton
-                aria-label="class setting menu"
-                onClick={handleClickMenu}
-              >
-                <MoreVertIcon />
-              </IconButton>
+        <Card variant="outlined" sx={{ cursor: 'pointer' }} >
+          <Link style={{ textDecoration: 'none', color: 'inherit' }} to={`/room/${data.classID}`} onClick={(e) => {
+            if (!allowNavigate) {
+              e.preventDefault(); // 如果不允许跳转，则阻止 Link 的默认行为
             }
-            title={data.title}
-          />
-          <Menu
-            id="long-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            sx={{
-              '& .MuiPaper-root': {
-                backgroundColor: '#222222',
-                color: 'white',
-              },
-            }}
-          >
-            <MenuItem onClick={handleChangeState}>{menuState == 'true' ? "Close" : "Open"}</MenuItem>
-            <MenuItem onClick={handleDelete} sx={{color:"#CC0000"}}>Delete</MenuItem>
-          </Menu>
-          <Link style={{ textDecoration: 'none', color: 'inherit' }} key={data.id} to={`/room/${data.classID}`}> 
-          <React.Fragment>
-            <CardContent >
+          }}>
+            <CardHeader
+              action={
+                <IconButton
+                  aria-label="class setting menu"
+                  onClick={handleClickMenu}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={data.title}
+            />
+            <Menu
+              id="long-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              sx={{
+                '& .MuiPaper-root': {
+                  backgroundColor: '#222222',
+                  color: 'white',
+                },
+              }}
+            >
+              <MenuItem onClick={handleChangeState}>{menuState == 'true' ? "Close" : "Open"}</MenuItem>
+              <MenuItem onClick={handleDelete} sx={{ color: "#CC0000" }}>Delete</MenuItem>
+            </Menu>
+            <React.Fragment>
+
+              <CardContent onClick={(e) => { if (!allowNavigate) e.stopPropagation(); }}>
                 <Typography sx={{ fontSize: 15, fontWeight: 600, marginTop: '12px' }} color={menuState == 'true' ? '#2D6CB6' : '#999999'} component="div">
                   state: {menuState == 'true' ? 'open' : 'close'}
-              </Typography>
-              <Typography sx={{ fontSize: 18, fontWeight: 600, }} color="#000" >
-                Class ID: {data.classID}
-              </Typography>
-            </CardContent>
-          </React.Fragment>
+                </Typography>
+                <Typography sx={{ fontSize: 18, fontWeight: 600, }} color="#000" >
+                  Class ID: {data.classID}
+                </Typography>
+              </CardContent>
+            </React.Fragment>
           </Link>
         </Card>
       </Box>
