11e8a4393016ffec9efa17654b46ca40f2666660
劉元楷 <Yuankai_@outlook.com>
2024-11-13 17:02:59 +0800

    add env config

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello Worlffd');
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+    res.send("Hello Worlffd");
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
