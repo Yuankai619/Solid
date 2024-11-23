387b648bec4be3af07327eca673aafd88d28b954
劉元楷 <Yuankai_@outlook.com>
2024-11-24 02:35:56 +0800

    add avatarUrl and googleId to auth.service

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
-        thumbnail: {
+        avatarUrl: {
             type: String,
             required: false,
         },
