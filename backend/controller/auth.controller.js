c09d26be2eaf783d27c29c7bffd1baec04691110
Yuankai619 <Yuankai_@outlook.com>
2024-11-27 04:28:48 +0800

    add findUser to auth.controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,9 +1,9 @@
 import authService from '../service/auth.service.js';
 export const signup = async (req, res) => {
     try {
-        console.log("req.body", req.body);
+        // console.log("req.body", req.body);
         const message = await authService.signup({ payload: req.body });
-        console.log("message", message);
+        // console.log("message", message);
         res.status(201).json({ message });
     } catch (error) {
         if (error.message === "User already exists") {
@@ -13,3 +13,14 @@ export const signup = async (req, res) => {
         }
     }
 };
+
+export const findUser = async (req, res) => {
+    try {
+        console.log("req.body", req.body);
+        const message = await authService.findUser({ payload: req.body });
+        console.log("message", message);
+        return res.status(200).json({ message });
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
