f3e0679bd2710a9407b40e534776c1fa22358703
Yuankai619 <Yuankai_@outlook.com>
2024-11-27 04:30:04 +0800

    add findUser route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { signup } from "../controller/auth.controller.js";
+import { signup, findUser } from "../controller/auth.controller.js";
+import ProtectRoute from "../middleware/ProtectRoute.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/user/find", ProtectRoute, findUser);
 
 export default router;
